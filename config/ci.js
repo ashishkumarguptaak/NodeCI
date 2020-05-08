473fba757d2d99e545c0544a9c8d1bb038b8d913
ashishkumarguptaak <ashishg00485@gmail.com>
2020-05-08 22:41:21 +0530

    Travis config

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID:
+      '437841298361-ik2v4cd021sdod76t1qbchf904q1cjos.apps.googleusercontent.com',
+    googleClientSecret: 'IrgmYp4coPN3qYKjcEEU_aq7',
+    mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+    cookieKey: '123123123',
+    redisUrl: 'redis://127.0.0.1:6379'
+  };
---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '437841298361-ik2v4cd021sdod76t1qbchf904q1cjos.apps.googleusercontent.com',
+  googleClientSecret: 'IrgmYp4coPN3qYKjcEEU_aq7',
+  mongoURI: 'mongodb://ashish:ASHISHashish123@ds361085.mlab.com:61085/blogs_dev',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
+};
